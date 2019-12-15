b7d7a6ceb097d98b6c94429af0a8da2c58f712dd
fedchenkoa <nsk.v.kadre@gmail.com>
2019-12-15 22:14:58 +0700

    Сделал сохранение настроек переключателей с помощью Shared Preferences

--- app/src/main/java/com/example/weather/Settings.java
+++ app/src/main/java/com/example/weather/Settings.java
@@ -1,13 +1,17 @@
 package com.example.weather;
 
+import android.content.SharedPreferences;
+import android.app.Activity;
+import static android.content.Context.MODE_PRIVATE;
 import android.os.Parcel;
 import android.os.Parcelable;
 
 public class Settings implements Parcelable {
-    private boolean humidityEnabled;
+    public boolean humidityEnabled;
     private boolean pressureEnabled;
     private boolean windSpeedEnabled;
     private String city;
+    SharedPreferences preferences;
 
     public Settings(boolean humidity, boolean pressure, boolean windSpeed, String city) {
         this.humidityEnabled = humidity;
@@ -35,32 +39,35 @@ public class Settings implements Parcelable {
         }
     };
 
-    public void setHumidityEnabled(boolean humidityEnabled) {
+    public void setHumidityEnabled(Activity activity, boolean humidityEnabled) {
         this.humidityEnabled = humidityEnabled;
+        activity.getSharedPreferences("Settings", MODE_PRIVATE).edit().putBoolean("humidity", humidityEnabled).apply();
     }
 
-    public void setPressureEnabled(boolean pressureEnabled) {
+    public void setPressureEnabled(Activity activity, boolean pressureEnabled) {
         this.pressureEnabled = pressureEnabled;
+        activity.getSharedPreferences("Settings", MODE_PRIVATE).edit().putBoolean("pressure", pressureEnabled).apply();
     }
 
-    public void setWindSpeedEnabled(boolean windSpeedEnabled) {
+    public void setWindSpeedEnabled(Activity activity, boolean windSpeedEnabled) {
         this.windSpeedEnabled = windSpeedEnabled;
+        activity.getSharedPreferences("Settings", MODE_PRIVATE).edit().putBoolean("windSpeed", windSpeedEnabled).apply();
     }
 
     public void setCity(String city) {
         this.city = city;
     }
 
-    public boolean isHumidityEnabled() {
-        return humidityEnabled;
+    public boolean isHumidityEnabled(Activity activity) {
+        return humidityEnabled = activity.getSharedPreferences("Settings", MODE_PRIVATE).getBoolean("humidity", true);
     }
 
-    public boolean isPressureEnabled() {
-        return pressureEnabled;
+    public boolean isPressureEnabled(Activity activity) {
+        return pressureEnabled  = activity.getSharedPreferences("Settings", MODE_PRIVATE).getBoolean("pressure", true);
     }
 
-    public boolean isWindSpeedEnabled() {
-        return windSpeedEnabled;
+    public boolean isWindSpeedEnabled(Activity activity) {
+        return windSpeedEnabled = activity.getSharedPreferences("Settings", MODE_PRIVATE).getBoolean("windSpeed", true);
     }
 
     public String getCity() {
